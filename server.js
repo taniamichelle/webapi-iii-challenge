3a05b65471d81c92dd71a520d63a5f8e79213461
tania <tanislavens@yahoo.com>
2019-09-04 16:40:26 -0400

    added get for users and userId

--- server.js
+++ server.js
@@ -1,15 +1,28 @@
-const express = 'express';
+const express = require('express');
+
+// import userRouter
+const userRouter = require('./users/userRouter');
 
 const server = express();
+// use express built-in middleware globally 
+server.use(express.json());
 
+// route handlers
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 });
 
-//custom middleware
+// use middleware locally
+server.use('/api/users', userRouter);
 
-function logger(req, res, next) {
+// use custom middleware locally
+// server.use('/users', validateUser, validateUserId, userRouter);
+// server.use('/posts', validatePost, postRouter);
 
-};
+// custom middleware
+// function logger(req, res, next) {
+
+// };
+
 
 module.exports = server;
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,47 +1,67 @@
-const express = 'express';
+const express = require('express');
+const router = express.Router('express');
+const db = require('../users/userDb');
 
-const router = express.Router();
-
-router.post('/', (req, res) => {
-
-});
-
-router.post('/:id/posts', (req, res) => {
-
-});
-
+// get all users
 router.get('/', (req, res) => {
-
+    db.get()
+        .then(user => res.status(200).json(user))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: "The users could not be retrieved." });
+        });
 });
 
+// router.post('/', (req, res) => {
+
+// });
+
+// get user by id
 router.get('/:id', (req, res) => {
-
+    const { id } = req.params;
+    db.getById(id)
+        .then((user) => {
+            // console.log(user);
+            if (user) {
+                res.status(200).json(user);
+            } else {
+                res.status(400).json({ error: "Invalid user id." });
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: "Cannot get user by id." });
+        });
 });
 
-router.get('/:id/posts', (req, res) => {
+// router.delete('/:id', (req, res) => {
 
-});
+// });
 
-router.delete('/:id', (req, res) => {
+// router.put('/:id', (req, res) => {
 
-});
+// });
 
-router.put('/:id', (req, res) => {
+// router.get('/:id/posts', (req, res) => {
 
-});
+// });
+
+// router.post('/:id/posts', (req, res) => {
+
+// });
 
 //custom middleware
 
-function validateUserId(req, res, next) {
+// function validateUserId(req, res, next) {
 
-};
+// };
 
-function validateUser(req, res, next) {
+// function validateUser(req, res, next) {
 
-};
+// };
 
-function validatePost(req, res, next) {
+// function validatePost(req, res, next) {
 
-};
+// };
 
 module.exports = router;
